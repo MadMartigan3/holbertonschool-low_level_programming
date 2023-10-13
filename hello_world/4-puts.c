526e5de5ab8f412d3869fae57bdc36911964e36f
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-13 01:35:19 -0700

    put/input

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char chaine[] = "Programming is like building a multilingual puzzle";
+puts(chaine);
+
+return (0);
+}
