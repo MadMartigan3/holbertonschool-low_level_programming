568124444955c4e361b4344fc5fcada469844cc3
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-16 03:11:59 -0700

    utilisation de if positif/negatif

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,26 +2,32 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
-int n;
+	int n = 10;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
 {
-printf("12 is positive");
+printf("10 is positive\n");
 }
 if (n == 0)
 {
-printf("0 is zero");
+printf("0 is zero\n");
 }
 if (n < 0)
 {
-printf("-12 is negative");
+printf("-10 is negative\n");
 }
 return (0);
 }
