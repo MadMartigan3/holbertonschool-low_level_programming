d64f9e2903294458166486e3d70a387a029a759b
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-17 04:25:21 -0700

    affichage de l'alphabet a l'envers avec putchar

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program that print the lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet --)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
