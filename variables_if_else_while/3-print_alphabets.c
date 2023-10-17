8441115eb646c703349232f90ef2eba0e17192dc
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-17 00:49:54 -0700

    utilisation de putchar pour afficher l'alphabet en maj et en min

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'use the command putchar for display the alphabet'
+ *
+ *@putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
