d160a86777173eb407f24d490d41e2a10af1322b
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-17 03:00:27 -0700

    afficher les nombres seule de 10 a 0

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that print all single digit
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
