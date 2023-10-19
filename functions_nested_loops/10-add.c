18a400ba66b21f4b452fb4d7f58aab1ee11876ac
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-19 04:22:38 -0700

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function that adds two integrers
+ *
+ *@n1: first number
+ *@n2: second number
+ *
+ * Return: Return the resultat
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
