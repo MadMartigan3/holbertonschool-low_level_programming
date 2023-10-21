ff8d36264e075b5d06af920be8deca8aea769e5b
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-21 02:24:32 -0700

    affichage diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line
+ *@n: numbers diago
+ *
+*/
+
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < j; i++)
+		{
+			for (j = 0; i < j; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
