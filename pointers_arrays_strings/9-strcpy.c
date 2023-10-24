af131998f018421a9a539631aca76c488452182a
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-24 07:16:51 -0700

    copier un string d'un pointeur a un autre

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - function that copies the string pointed
+ *@dest: input pointer destination
+ *@src: input pointer source
+ *
+ * Return: destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; *(src + x) != 0; x++)
+	{
+		*(dest + x) = *(src + x);
+	}
+	*(dest + x) = '\0';
+	return (dest);
+}
