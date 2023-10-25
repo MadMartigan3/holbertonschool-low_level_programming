f1611751cafc4837b21dd60090aa7990758fb043
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-25 06:55:56 -0700

    concatener 2 strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,18 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i =0; dest[i]; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i])
 	{
+		i++;
 	}
-	for (j = 0; j < n && src[j]; j++)
+	while (j < n && src[j])
 	{
 		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 	return (dest);
