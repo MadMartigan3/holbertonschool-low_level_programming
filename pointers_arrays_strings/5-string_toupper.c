9b2ad9ed326fdf126a489bafa2014fae24d3454b
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-26 07:20:46 -0700

    fonction qui change les lettres minuscules en majuscules

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a strings
+ *@str: input string
+ *
+ * Return: value of str
+ *
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
