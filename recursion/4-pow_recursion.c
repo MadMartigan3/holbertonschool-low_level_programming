ca05a39d280d627571465011d3f1d02be3605a19
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-10-30 03:24:17 -0700

    fonction qui retourne le resultat de x puissance y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that return the value of x power y
+ *@x: number
+ *@y: puissance
+ *
+ * Return: success 0.
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (_pow_recursion(x, y - 1) * x);
+}
