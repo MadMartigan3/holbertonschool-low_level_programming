7acf4c461d66436c34ead58ced0e224c20b47f5d
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-11-09 04:13:41 -0800

    fonction qui creer un tableau d'entiers

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function that creates an array of integers
+ *@min: smallest number in array
+ *@max: largest value in array
+ *
+ *Return: a success.
+ */
+
+int *array_range(int min, int max)
+{
+	int *a;
+	int i, j;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	a = malloc(sizeof(*a) * ((max - min) + 1));
+
+	if (a != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			a[j] = i;
+			j++;
+		}
+		return (a);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
