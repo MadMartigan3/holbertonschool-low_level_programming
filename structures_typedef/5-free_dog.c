2d83edf59bedd550d346e1af162d87108fb623bf
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-11-15 02:01:15 -0800

    correction 4/5 projet 0

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - function qui libere la memoire alloué
+ *@d: structure
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
