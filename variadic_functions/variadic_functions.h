702987c971ea2fff64ce86aa84b882bb980425fd
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-11-17 08:20:37 -0800

    fonction qui affiche tout

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,16 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
+typedef struct method
+{
+	char letter;
+	void (*print)(va_list ap);
+} choice;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
