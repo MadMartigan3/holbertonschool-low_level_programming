8efae284916d391034587fef2137a9becb9d356d
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-11-17 04:23:22 -0800

    fonction qui affiche des nombres

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function that print numbers
+ *@separator: thing to pront between arg
+ *@n: numbers of args
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
