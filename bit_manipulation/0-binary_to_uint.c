667be5c3da44104e7c0668dd7a4cc78ce02faef4
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-12-01 14:31:21 +0100

    projet 2 task 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary to an int
+ * @b: pointer
+ * 
+ * Return: the converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int val = 0;
+    int i = 0;
+
+    if (b == NULL)
+    {
+        return (0);
+    }
+
+    while (b[i] == '0' || b[i] == '1')
+    {
+        val <<= 1;
+        val += b[i]-'0';
+        i++;
+    }
+    return (val);
+}
---
+++ bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
+
+
+unsigned int binary_to_uint(const char *b);
+#endif
