c05f109e7b37d2996d0a46f2b6eac45b3a609c35
MadMartigan3 <gabrielmilan3@hotmail.com>
2023-12-08 09:10:20 +0100

    projet 1 tasK 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,7 +1,10 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_create - function that create a hash table
+ *  @size: size of array
  * 
+ * Return: new hash table
 */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -9,6 +12,24 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_table_t *nt;
     unsigned long int i;
 
-    if ()
+    nt = malloc(sizeof(hash_node_t));
 
+    if (nt == NULL)
+    {
+        return (NULL);
+    }
+    nt->size = size;
+
+    nt->array = malloc(sizeof(hash_node_t *) * size);
+
+    if (nt->array == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        nt->array[i] = NULL;
+    }
+    return (nt);
 }
