688dc19e169314b29f4009e0cd613d21a5d83138
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-19 16:51:15 +0100

    Projet 1 taask 0 correct

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+*
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL || !value)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
+			return (i);
+		}
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	}
+	return (-1);
+}
