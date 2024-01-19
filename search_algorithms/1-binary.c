9bfd6a8fb3d6ed595cd1f938645d2777152fa917
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-19 17:44:18 +0100

    taask 2 projet 1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+
+/**
+*binary_search - search of a value in an array
+*@array: pointer
+*@size: number element
+*@value: value search
+*Return: index i
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i;
+	int low = 0;
+	int high = size - 1;
+	int mid;
+
+	if (array == NULL || !value)
+	{
+		return (-1);
+	}
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+
+		for (i = 0; i < size; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			low = mid + 1;
+		}
+		else
+		{
+			high = mid - 1;
+		}
+	}
+	return (-1);
+}
